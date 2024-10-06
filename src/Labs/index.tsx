131001813088114f9d724e5c4df40a5080b0516f
grayson6 <cooper.gr@northeastern.edu>
2024-10-05 22:03:58 -0400

    a2 CSS and Bootstrap

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -12,6 +12,9 @@ export default function Labs() {
       <h1>Labs</h1>
       <p> Name: Grayson Cooper; CS5610 FA24 Async Online </p>
       <TOC />
+      <a id="wd-github" href="https://github.com/grayson6/kanbas-react-web-app" target="_blank" rel="noopener noreferrer">
+        View on GitHub
+      </a>
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
@@ -21,3 +24,27 @@ export default function Labs() {
     </div>
   );
 }
+
+// // src/Labs/index.tsx
+
+// import { Routes, Route, Navigate } from 'react-router-dom';
+// import TOC from './TOC';
+// import Lab1 from './Lab1';
+// import Lab2 from './Lab2';
+// import Lab3 from './Lab3';
+
+// export default function Labs() {
+//   return (
+//     <div>
+//       <h1>Labs</h1>
+//       <p> Name: Grayson Cooper; CS5610 FA24 Async Online </p>
+//       <TOC />
+//       <Routes>
+//         <Route path="/" element={<Navigate to="Lab1" />} />
+//         <Route path="Lab1" element={<Lab1 />} />
+//         <Route path="Lab2" element={<Lab2 />} />
+//         <Route path="Lab3" element={<Lab3 />} />
+//       </Routes>
+//     </div>
+//   );
+// }
